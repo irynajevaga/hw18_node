0add1a09cb32df7be2748e138be16a1b1d04f9ea
irynajevaga <163996627+irynajevaga@users.noreply.github.com>
2024-10-26 23:56:13 +0200

    Initial commit

---
+++ 1.ts
@@ -0,0 +1,20 @@
+type Admin = {
+  name: string;
+  permissions: string[];
+}
+
+type User = {
+  name: string;
+  email: string;
+}
+
+type AdminUser = Admin & User
+
+const userAdmin : AdminUser = {
+  name: 'John',
+  permissions: ['read', 'write', 'delete'],
+  email: 'user@admin.test'
+
+}
+
+console.log(userAdmin);
